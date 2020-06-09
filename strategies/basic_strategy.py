7d3983f88d0930b95c6777dbd730b7bbfc8ed515
draliii <holl.dita@gmail.com>
2020-06-09 19:21:44 +0200

    Fixed the targeted strategy

--- strategies/basic_strategy.py
+++ strategies/basic_strategy.py
@@ -10,7 +10,9 @@ def attack_all(round_no, number_of_peers):
 
 
 def attack_p1(round_no, number_of_peers):
-    attack_plan = [Attack.Benign] * number_of_peers
     if round_no < 200:
+        attack_plan = [Attack.TargetedOnOthers] * number_of_peers
         attack_plan[1] = Attack.TargetedAttack
+    else:
+        attack_plan = [Attack.Benign] * number_of_peers
     return attack_plan
