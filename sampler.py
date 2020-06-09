4db53917f11d4aafa4f991811eb22d7d7461544f
draliii <holl.dita@gmail.com>
2020-06-09 18:56:01 +0200

    Fixed bugs, now the demo works for two peers

--- sampler.py
+++ sampler.py
@@ -22,6 +22,11 @@ def get_score_sample(action: Attack):
     if action == Attack.TargetedOnOthers:
         return np.random.beta(3, 3)
 
+
+def clean_floats(new_score, new_confidence):
+    return round(new_score, 5), round(new_confidence, 5)
+
+
 class Sampler:
     def __init__(self, n):
         self.n = n
@@ -44,12 +49,14 @@ class Sampler:
         for peer_no in range(0, self.n):
             # this is a line that tells me how everyone will treat peer with id peer_no
             for remote_peer_no in range(0, self.n):
-                action = attack_matrix[peer_no][remote_peer_no]
+                if peer_no == remote_peer_no:
+                    continue
+                action = attack_matrix[remote_peer_no][peer_no]
                 new_score = get_score_sample(action)
                 self.update_scores(peer_no, remote_peer_no, new_score)
 
     def update_scores(self, peer_no, remote_peer_no, score):
-        history = self.peer_data[peer_no][remote_peer_no]
+        history = self.peer_data[remote_peer_no][peer_no]
         last_score, last_confidence = history[-1]
 
         # get difference in scores
@@ -58,16 +65,17 @@ class Sampler:
         # is the current score similar to the latest one?
         if dif < 0.1:
             # if yes, confidence should go up
-            new_confidence = max(1, last_confidence + 0.1)
+            new_confidence = min(1, last_confidence + 0.1)
             # score should move to the average of the last two scores
             new_score = (score + last_score) / 2
         else:
             # it the difference is high, confidence will be lowered
-            new_confidence = min(0, last_confidence - 0.1)
+            new_confidence = max(0, last_confidence - 0.1)
             if score < last_score:
                 # if new data suggests a bad peer is good, higher priority is given to old data, to make bad reputation hard to lose
                 new_score = (score + 2 * last_score) / 3
             else:
-                new_score = (score + last_score / 2)
+                new_score = (score + last_score) / 2
 
-        self.peer_data[peer_no][remote_peer_no].append((new_score, new_confidence))
+        new_score, new_confidence = clean_floats(new_score, new_confidence)
+        self.peer_data[remote_peer_no][peer_no].append((new_score, new_confidence))
