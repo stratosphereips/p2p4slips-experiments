f45a965cb61bae26a045d856369ebf18caa55f4f
draliii <holl.dita@gmail.com>
2020-06-11 15:20:37 +0200

    Added structure for strategy

--- strategies/basic_strategy.py
+++ strategies/basic_strategy.py
@@ -16,3 +16,17 @@ def attack_p1(round_no, number_of_peers):
     else:
         attack_plan = [Attack.Benign] * number_of_peers
     return attack_plan
+
+class Strategy:
+
+    def __init__(self):
+        pass
+
+    def on_round_start(self, round_no):
+        pass
+
+    def choose_round_behavior(self, round_no):
+        pass
+
+    def on_round_end(self, round_no):
+        pass
