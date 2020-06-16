7a4fbb7e2f0bdc79fdacd744b40077ef4780ea03
draliii <holl.dita@gmail.com>
2020-06-16 19:16:23 +0200

    Fixed pigeon message forwarding

--- dovecot.py
+++ dovecot.py
@@ -3,6 +3,7 @@
 # forward slips updates to peers
 # listen to messages from all peers
 # send messages to other peers
+import json
 import time
 import multiprocessing
 
@@ -23,6 +24,7 @@ class Dovecot(multiprocessing.Process):
 
         self.peers = peers
         self.peers_by_name = {}
+        self.peers_by_port = {}
 
         # "p2p_gopy" this is where pigeon sends messages to the module core (GoListener is taking care of that)
         # "p2p_data_request" is a channel when Slips asks for network opinion - this will be used to get results out of the network
@@ -33,6 +35,7 @@ class Dovecot(multiprocessing.Process):
         for peer in self.peers:
             self.pubsub.subscribe(peer.pygo_channel)
             self.peers_by_name[peer.name] = peer
+            self.peers_by_port[peer.port] = peer
 
         outgoing_channel_types = ["p2p_gopy", "p2p_data_request", "ip_info_change"]
 
@@ -59,24 +62,30 @@ class Dovecot(multiprocessing.Process):
                     continue
 
                 data = message['data']
+
+                sender = self.get_sender_from_channel(channel)
+                dict_data = json.loads(data)
+                self.forward_message_to_peer(sender.name, dict_data)
                 print("Message contents: ", data)
         except:
             pass
 
-    def send_string_to_peer_name(self, peer_name, send_string):
+    def send_string_to_peer_name(self, source_peer_name, peer_name, send_string):
         if peer_name == "*":
             for single_peer_name in self.peers_by_name.keys():
-                self.send_string_to_peer_name(single_peer_name, send_string)
+                self.send_string_to_peer_name(source_peer_name, single_peer_name, send_string)
             return
-        else:
-            peer = self.peers_by_name[peer_name]
-            self.send_string_to_peer(peer, send_string)
+        if peer_name == source_peer_name:
+            return
+
+        peer = self.peers_by_name[peer_name]
+        self.send_string_to_peer(peer, send_string)
 
     def send_string_to_peer(self, peer: PeerWithStrategy, send_string: str):
-        channel_name = peer.gopy_channel()
+        channel_name = peer.gopy_channel
         __database__.publish(channel_name, send_string)
 
-    def forward_message_to_peer(self, source_peer_name, message_data):
+    def forward_message_to_peer(self, source_peer_name, message_data: dict):
         # {"message": "ewogICAgIm........jYKfQ==","recipient": "peer_name_goes_here"}
 
         # go adds some data to the message:
@@ -89,11 +98,27 @@ class Dovecot(multiprocessing.Process):
         # ]
 
         data_content = [{"reporter": source_peer_name, "report_time": time.time(), "message": message_data["message"]}]
-        self.send_string_to_peer_name(message_data["recipient"], str(data_content))
+        message_str = "go_data " + json.dumps(data_content)
+        self.send_string_to_peer_name(source_peer_name, message_data["recipient"], message_str)
         pass
 
-    def send_reliability_to_peer(self, local_peer_name, remote_peer_name, peer_ip, reliability, timestamp):
-        update_data = {"peerid": remote_peer_name, "ip": peer_ip, "reliability": reliability, "timestamp": timestamp}
-        message = "peer_update " + str(update_data)
+    def send_reliability_to_peer(self, local_peer_name, remote_peer_name, reliability, timestamp):
+        update_data = {"peerid": remote_peer_name, "reliability": reliability, "timestamp": timestamp}
+        message = "peer_update " + json.dumps(update_data)
 
-        self.send_string_to_peer_name(local_peer_name, message)
+        self.send_string_to_peer_name("*", local_peer_name, message)
+
+    def get_sender_from_channel(self, channel):
+        # read port from channel name and return the peer that owns this port
+        for i, c in enumerate(channel):
+            if not c.isdigit():
+                continue
+            port_str = channel[i:]
+            try:
+                port = int(port_str)
+                peer = self.peers_by_port[port]
+                return peer
+            except:
+                continue
+        print("Couldn't find peer running on channel " + channel)
+
