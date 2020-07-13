99a4d8552153f76ab35b6ca3aeb14c4eaa5b8e3f
draliii <holl.dita@gmail.com>
2020-07-13 19:16:23 +0200

    Prepared experiments with varying amount of bad peers

--- setups.py
+++ setups.py
@@ -1,10 +1,20 @@
 import configparser
 import os
+import time
+from multiprocessing import Queue
 
+from outputProcess import OutputProcess
 from p2ptrust.testing.experiments.controller import Controller
 from p2ptrust.testing.experiments.custom_devices.device_malicious import DeviceMalicious
 from p2ptrust.testing.experiments.custom_devices.peer import Peer
 from p2ptrust.testing.experiments.custom_devices.peer_liar_everyone_is_good import PeerLiarEveryoneIsGood
+from slips.core.database import __database__
+
+
+def get_default_config():
+    cfg = configparser.ConfigParser()
+    cfg.read_file(open("../../../../slips.conf"))
+    return cfg
 
 
 class Setups:
@@ -41,3 +51,55 @@ class Setups:
 
         ctrl = Controller(peers, 3, ["1.1.1.0"], ["1.1.1.1"], data_dir)
         return ctrl
+
+    def keep_malicious_device_unblocked(self, output_process_queue, config: configparser.ConfigParser):
+        # how many attackers does it take to keep communicating with a malicious device?
+        # there are 10 peers total, 0-9 of them are malicious.
+        # There is a malicious device that is attacking everyone except peer 1
+        # after 10 round, this device starts attacking everyone.
+        n_peers = 10
+        for n_malicious_peers in range(0, n_peers):
+            data_dir = self.data_dir + str(n_malicious_peers) + "_keep_malicious_device_unblocked/"
+            os.mkdir(data_dir)
+            devices = []
+            n_good_peers = n_peers - n_malicious_peers
+            for peerid in range(0, n_peers):
+                port = 6660 + peerid
+                ip_address = "1.1.1." + str(peerid)
+                if peerid < n_good_peers:
+                    # create a good peer
+                    p = Peer(output_queue=output_process_queue,
+                             config=config,
+                             data_dir=data_dir,
+                             port=port,
+                             ip_address=ip_address,
+                             name=str(peerid) + "_peer_benign")
+                else:
+                    # create a bad peer
+                    p = PeerLiarEveryoneIsGood(output_queue=output_process_queue,
+                                               config=config,
+                                               port=port,
+                                               data_dir=data_dir,
+                                               ip_address=ip_address,
+                                               name=str(peerid) + "_peer_malicious")
+                devices.append(p)
+
+            p = DeviceMalicious(ip_address="1.1.1." + str(n_peers), name=str(n_peers) + "_device_malicious", is_good=False)
+            devices.append(p)
+            k = 3
+
+
+if __name__ == '__main__':
+    config = get_default_config()
+    output_process_queue = Queue()
+    output_process_thread = OutputProcess(output_process_queue, 1, 1, config)
+    output_process_thread.start()
+
+    # Start the DB
+    __database__.start(config)
+    __database__.setOutputQueue(output_process_queue)
+    base_dir = "/home/dita/ownCloud/stratosphere/SLIPS/modules/p2ptrust/testing/experiments/experiment_data/experiments-" + str(
+        time.time()) + "/"
+    os.mkdir(base_dir)
+    s = Setups(base_dir)
+    s.keep_malicious_device_unblocked(output_process_queue, config)
