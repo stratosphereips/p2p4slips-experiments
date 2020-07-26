faf882de68d108f9c1d735735c569458a8f2795d
draliii <holl.dita@gmail.com>
2020-07-26 16:25:07 +0200

    tables are now generated with accuracies

--- evaluator.py
+++ evaluator.py
@@ -13,8 +13,11 @@ def evaluate_ips_only(observations: dict, rounds, is_good=None, threshold=-0.5):
     tn = 0  # correctly identified benign ips
     fp = 0  # falsely accused benign ips
     fn = 0  # undetected attackers
+    observed_ips = list(is_good.keys())
 
     lines = {k: "" for k in is_good.keys()}
+    observation_results = {k: {"FP": 0, "TP": 0, "FN": 0, "TN": 0} for k in is_good.keys()}
+
     for rnd in range(0, rounds):
         try:
             rnd_data = observations[rnd]
@@ -33,30 +36,24 @@ def evaluate_ips_only(observations: dict, rounds, is_good=None, threshold=-0.5):
                 if decision < threshold:
                     # we say it is an attacker
                     if gt:
-                        lines[observed_ip] += "FP"
-                        fp += 1  # we are wrong
+                        result = "FP"  # we are wrong
                     else:
-                        lines[observed_ip] += "TP"
-                        tp += 1  # we are right
+                        result = "TP"  # we are right
                 else:
                     # we say he is benign
                     if gt:
-                        lines[observed_ip] += "TN"
-                        tn += 1  # we are right
+                        result = "TN"  # we are right
                     else:
-                        lines[observed_ip] += "FN"
-                        fn += 1  # we are wrong
+                        result = "FN"  # we are wrong
 
-    observed_ips = list(is_good.keys())
+                observation_results[observed_ip][result] += 1
+                lines[observed_ip] += result
+
+
     print(lines[observed_ips[0]])
     print(lines[observed_ips[1]])
 
-    print("STATS: ", tp, tn, fp, fn)
-    success = tp + tn
-    all = tp + tn + fp + fn
-    accuracy = success / all
-    print("ACCURACY: ", accuracy)
-    return accuracy
+    return observation_results
 
 
 def evaluate(observations: dict, ipdb: IPDatabase, rounds, is_good=None, threshold=-0.5):
@@ -132,12 +129,87 @@ def eval_one_exp(exp_dir, exp_id, exp_suffix, is_good):
                 accuracy = evaluate_ips_only(data, 20, is_good, threshold=threshold)
                 accuracies[threshold].append(accuracy)
 
-        find_best_threshold(accuracies)
+        find_best_threshold_long_table(accuracies)
 
 
-def find_best_threshold(accuracies: dict):
-    for t in accuracies.keys():
-        print("Acc for t " + str(t) + " is " + str(accuracies[t]))
+def find_best_threshold_wide_table(observation_results: dict):
+    thresholds = sorted(list(observation_results.keys()))
+    first_threshold = thresholds[0]
+    observed_ips = list(observation_results[first_threshold].keys())
+
+    lines = {observed_ip: str(observed_ip) + " & " for observed_ip in observed_ips}
+
+    lines["all"] = "All & "
+
+    thresholds_line = "".join([str(t) + " & " for t in thresholds])
+
+
+    for threshold in thresholds:
+        success_t = 0
+        total_t = 0
+        for observed_ip in observed_ips:
+            x = observation_results[threshold][observed_ip]  # alias
+            success_i = x["TP"] + x["TN"]
+            total_i = x["TP"] + x["TN"] + x["FP"] + x["FN"]
+            success_t += success_i
+            total_t += total_i
+            accuracy = success_i/total_i
+            lines[observed_ip] += str(accuracy) + " & "
+
+        accuracy = success_t / total_t
+        lines["all"] += str(accuracy) + " & "
+
+    print("Thresholds" + thresholds_line[:-2] + "\\\\")
+    print("\\hline")
+    for ip in lines.keys():
+        print(lines[ip][:-2] + "\\\\")
+        print("\\hline")
+
+
+def find_best_threshold_long_table(observation_results: dict):
+    thresholds = sorted(list(observation_results.keys()))
+    first_threshold = thresholds[0]
+    observed_ips = list(observation_results[first_threshold].keys())
+
+    lines = {observed_ip: str(observed_ip) + " & " for observed_ip in observed_ips}
+
+    lines["all"] = "All & "
+
+    thresholds_line = "".join([str(t) + " & " for t in thresholds])
+
+    top_line = "Threshold & " + "".join([ip + " & " for ip in observed_ips]) + "All \\\\"
+    print(top_line)
+    print("\\hline")
+    print("\\hline")
+
+    for threshold in thresholds:
+        success_t = 0
+        total_t = 0
+
+        t_line = ""
+        t_line += str(threshold) + " & "
+        for observed_ip in observed_ips:
+            x = observation_results[threshold][observed_ip]  # alias
+            success_i = x["TP"] + x["TN"]
+            total_i = x["TP"] + x["TN"] + x["FP"] + x["FN"]
+            success_t += success_i
+            total_t += total_i
+            accuracy = success_i/total_i
+            t_line += str(accuracy) + " & "
+
+        accuracy = success_t / total_t
+        t_line += str(accuracy) + " \\\\"
+        print(t_line)
+        print("\\hline")
+
+
+    #
+    # print("Threshold & Accuracy \\\\")
+    # print("\\hline")
+    # for t in observation_results.keys():
+    #     print("\\hline")
+    #     print(str(t) + " & " + str(observation_results[t]) + " \\\\")
+    # print("\\hline")
 
 
 def eval_exp_ips_only():
@@ -148,8 +220,6 @@ def eval_exp_ips_only():
     is_good = {"1.1.1.10": False, "1.1.1.11": True}
     thresholds = [x / 10 for x in range(-10, 11)]
     accuracies = {}
-    for t in thresholds:
-        accuracies[t] = []
 
     data_file = exp_dir + str(exp_id) + exp_suffix + "/round_results.txt"
     with open(data_file, "r") as f:
@@ -157,10 +227,9 @@ def eval_exp_ips_only():
         for threshold in thresholds:
             print("Experiment id: " + str(exp_id) + "/10, threshold = " + str(threshold))
             accuracy = evaluate_ips_only(data, 20, is_good, threshold=threshold)
-            accuracies[threshold].append(accuracy)
+            accuracies[threshold] = accuracy
 
-    find_best_threshold(accuracies)
-
+    find_best_threshold_long_table(accuracies)
 
 
 if __name__ == '__main__':
@@ -169,7 +238,7 @@ if __name__ == '__main__':
     exp_dir = "/home/dita/p2ptrust-experiments-link/experiment_data/experiments-1594815406.4306164_/"
     exp_suffix = "_keep_malicious_device_unblocked"
 
-    eval_exp_ips_only()
+    observed_results_ = eval_exp_ips_only()
 
     # exp_dir = "/home/dita/p2ptrust-experiments-link/experiment_data/experiments-1595605824.1189618/"
     # exp_suffix = "_attacker_targeting_different_amounts_of_peers"
