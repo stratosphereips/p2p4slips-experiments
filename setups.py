5a5fead0cef8f40932a9e62bfc040c49986ca637
draliii <holl.dita@gmail.com>
2020-07-27 21:03:47 +0200

    Added exp 3c

--- setups.py
+++ setups.py
@@ -264,6 +264,26 @@ class Setups:
             ctrl.run_experiment()
             time.sleep(5)
 
+    def run_3c(self, dir_prefix):
+        # malicious peers are praising the malicious device
+
+        base_dir = prepare_experiments_dir(dir_prefix, exp_name="_exp_3b")
+
+        for n_good_peers in range(1, 10):
+            for n_victims in range(1, 10):
+                exp_id = 10*n_good_peers + n_victims
+                attack_plan = get_attack_plan_with_given_victim_count(n_rounds=20, n_victims=n_victims)
+                ctrl = self.attack_parametrised(base_dir,
+                                                exp_id=exp_id,
+                                                n_good_peers=n_good_peers,
+                                                n_peers=10,
+                                                n_rounds=20,
+                                                attack_plan=attack_plan,
+                                                bad_peer_type="PeerLiarEveryoneIsGood",
+                                                experiment_suffix="")
+                ctrl.run_experiment()
+                time.sleep(5)
+
     def run_4(self, dir_prefix):
         # badmouthing
 
